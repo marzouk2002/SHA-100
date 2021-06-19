11cf3b98ece9461e105365f05aeacaceb5f74ab3
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-19 20:17:15 +0100

    format done

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,14 @@ function main(inputStr) {
 function format(str) {
     var binaryStr = str.split('').map(function (char) { return char.charCodeAt(0).toString(2); }).join('');
     var toOneHundred = 100 - binaryStr.length % 100;
-    return [binaryStr];
+    var zerosToAdd = Array(toOneHundred).fill(0).join('');
+    binaryStr = zerosToAdd + binaryStr;
+    var binaryArr = binaryStr.split('');
+    var toReturn = [];
+    while (binaryArr.length) {
+        toReturn.push(binaryArr.splice(0, 100).join(''));
+    }
+    console.log(toReturn);
+    return toReturn;
 }
 exports.default = main;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function main(inputStr: string):any {
+function main(inputStr: string):Array<string> {
     return format(inputStr)
 };
 
@@ -7,8 +7,18 @@ function format(str: string):Array<string> {
     let binaryStr = str.split('').map(char => char.charCodeAt(0).toString(2)).join('')
     
     const toOneHundred: number = 100 - binaryStr.length % 100 
+    const zerosToAdd: string = Array(toOneHundred).fill(0).join('')
+    binaryStr = zerosToAdd + binaryStr
 
-    return [binaryStr]
+    const binaryArr: Array<string> = binaryStr.split('');
+    const toReturn:Array<string> = []
+
+    while(binaryArr.length) {
+        toReturn.push(binaryArr.splice(0, 100).join(''))
+    }
+    console.log(toReturn)
+
+    return toReturn
 }
 
 export default main
